c5e462177d00e49f7ec0d07914af6b5fa0d02048
Justin Yoo <justinyoo@fb.com>
2018-10-23 08:43:09 -0700

    Add tests for card number and cvv2 validation

--- tests/PaymentFormValidationUtils.test.js
+++ tests/PaymentFormValidationUtils.test.js
@@ -5,10 +5,16 @@ const {
   validateName,
 } = require('../src/utils/PaymentFormValidationUtils');
 
-test('Validate card numb', () => {
+test('Validate card number', () => {
   expect(validateCardNumber('4147100412345678')).toEqual(true);
   expect(validateCardNumber('41471004')).toEqual(false);
   expect(validateCardNumber('1234567891012131')).toEqual(false);
   expect(validateCardNumber('')).toEqual(false);
   expect(validateCardNumber('srtdtrdtr')).toEqual(false);
+});
+
+test('Validate card number', () => {
+  expect(validateCvv2Number('123', '4147100412345678')).toEqual(true);
+  expect(validateCvv2Number('1234', '344710041234567')).toEqual(true);
+  expect(validateCvv2Number('1234', '344710041234567')).toEqual(true);
 });
